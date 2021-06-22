68533f7a301c07a1164435fd715173c9c47d2c1a
Irene-Busah <i.busah@alustudent.com>
2021-06-22 23:34:37 +0200

    digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,25 @@
 #include "holberton.h"
-
 /**
- *print_last_digit - display last digit
- *@n: parameter integer
- *Return: last digit
+ * print_last_digit - Prints the last digit
+ * of a number.
+ *
+ * @n: input number as an integer.
+ *
+ * Return: last digit.
  */
 int print_last_digit(int n)
 {
-	int result;
-
-	result = n % 10;
-
-	if (n < 0)
-	{
-		result = -1;
-	}
-
-	_putchar(result + '0');
-
-	return (result);
+int l;
+l = n % 10;
+if (l < 0)
+{
+_putchar(-l + 48);
+return (-l);
 }
+else
+{
+_putchar(l + 48);
+return (l);
+}
+}
+
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,17 +1,21 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int n);
-int _print_last_digit(int n);
-int add(int, int);
-void print_to_98(int n);
-void print_alphabet(void);
-void print_alphabet_x10(void);
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-
+void pr_digit(int charcode, int fmt);
+int add(int a, int b);
+void pr_with_fmt(int charcode, int fmt);
+void print_to_98(int n);
+void pr_3_digit(int charcode, int fmt);
+void print_times_table(int n);
+void print_spaces(int n);
+void print_zero(void);
 #endif
