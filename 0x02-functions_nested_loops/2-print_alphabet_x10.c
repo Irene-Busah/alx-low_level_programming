1b47d083170e10f28103f6dc0effd4ac69abfb2b
Irene-Busah <i.busah@alustudent.com>
2021-06-22 15:13:24 +0200

    print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_10x - display alphabet
+ *Return: void
+ */
+void print_alphabet_10x(void)
+{
+  int i;
+  int c;
+
+  for (i = 0; i <= 0; i++)
+    {
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	  _putchar(c);
+	}
+      _putchar('\n');
+    }
+}
