114874f3d761c969d55e8da38b19e4b48b1df186
Irene-Busah <i.busah@alustudent.com>
2021-07-05 16:10:11 +0200

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+/**
+ *_strchr - locates a character in a string.
+ *@s: pointer parameter char
+ *@c: parameter character
+ *Return: s variable
+ */
+char *_strchr(char *s, char c)
+{
+	int i, n;
+
+	while (s[n])
+		n++;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (s[i] == c)
+		{
+			s = &s[i];
+			break;
+		}
+
+		if (s[i] == '\0')
+			s = '\0';
+	}
+
+	return (s);
+}
