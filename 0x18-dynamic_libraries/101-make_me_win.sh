62da2e09c26bc6504b165c88e273520f7f051313
Irene-Busah <i.busah@alustudent.com>
2021-09-20 20:39:47 +0200

    new

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Irene-Busah/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=/tmp/libwinner.so
+cp ../sh2.sh .
+. ./sh2.sh
